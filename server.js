613dc49a67b9e4f20a24efd1a3d2a3f4652231b4
long phan <lphan1989@gmail.com>
2016-05-12 20:13:26 -0400

    forgot to commit huehehu

--- server.js
+++ server.js
@@ -4,9 +4,21 @@ var session = require('express-session');
 var bodyParser = require("body-parser");
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var index = require('./routes/index.js')
+var PORT = process.env.PORT || 3000
 
-
-
-app.use("/", express.static("public"));
+// app.use("/", express.static("public"));
 app.use("/js", express.static("public/js"))
 app.use("/css", express.static("public/css"))
+app.use(express.static(__dirname + "views"))
+app.use(express.static(__dirname + "public"))
+
+
+
+app.use('/', index);
+
+
+
+app.listen(PORT, function() {
+  console.log(PORT + ' listening on')
+})
