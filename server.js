5595feb480d1b3318ec5566c0f8157d42087a5ef
longp <lphan1989@gmail.com>
2016-05-20 12:31:29 -0400

    added signup page and ctrl also added some npm packatges to serverjs

--- server.js
+++ server.js
@@ -3,16 +3,29 @@ var app = express();
 var session = require('express-session');
 var bodyParser = require("body-parser");
 var logger = require('morgan');
+var cors = require('cors');
+var jwt = require('jwt-simple');
+var path = require('path');
+var request = require('request');
 var mongoose = require('mongoose');
 var index = require('./routes/index.js')
 var PORT = process.env.PORT || 3000
 
-// app.use("/", express.static("public"));
-app.use("/js", express.static("public/js"))
-app.use("/css", express.static("public/css"))
-app.use("/partials", express.static("public/partials"))
-app.use(express.static(__dirname + "views"))
-app.use(express.static(__dirname + "public"))
+
+
+app.set('port', process.env.PORT || 3000);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// // app.use("/", express.static("public"));
+// app.use("/js", express.static("public/js"))
+// app.use("/css", express.static("public/css"))
+// app.use("/partials", express.static("public/partials"))
+// app.use(express.static(__dirname + "views"))
+// app.use(express.static(__dirname + "public"))
 
 
 
